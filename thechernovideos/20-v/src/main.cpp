4cc6a0cbb1811032cd20fd5fdf24edd22e740255
Carlos12001 <62664256+Carlos12001@users.noreply.github.com>
2022-02-12 12:01:46 -0600

    V20 finished

---
+++ thechernovideos/20-v/src/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+class Logger{
+
+public:
+
+	int levelError = 1;
+
+	int levelWarning = 2;
+
+	int levelInfo = 3;
+
+private:
+
+	int myLevel = levelInfo;
+
+public:
+
+	void setLevel(int level){
+		myLevel = level;
+		return;
+	}
+
+	void Info(const char *message){
+		if (myLevel >= levelInfo)
+			std::cout << "Info: " << message << std::endl;
+		return;
+	}
+
+	void Warning(const char *message){
+		if (myLevel >= levelWarning)
+			std::cout << "Warning: " << message << std::endl;
+		return;
+	}
+
+	void Error(const char *message){
+		if (myLevel >= levelError)
+			std::cout << "Error: " << message << std::endl;
+		return;
+	}
+
+};
+
+int main(){
+	Logger log;
+	log.setLevel(log.levelWarning);
+	log.Info("Information");
+	log.Warning("Warn!!");
+	log.Error("Failed :(");
+	return 1;
+}
